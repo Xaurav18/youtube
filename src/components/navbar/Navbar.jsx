308ed3091ca86da6b892b039a9c4a451c381e90b
Xaurav18 <bajracharyasaurav22@gmail.com>
2021-12-01 17:47:25 +0545

    .

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,8 +5,15 @@ const NavBar = styled.div`
   background-color: #0b0b0b;
   color: white;
 `;
-function Navbar() {
-  return <NavBar>Navbar</NavBar>;
-}
 
+function Navbar() {
+  return (
+    <NavBar>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+        alt=""
+      />
+    </NavBar>
+  );
+}
 export default Navbar;
