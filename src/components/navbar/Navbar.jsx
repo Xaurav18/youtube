d5c727145a1fb4c51b341d2be640864d7d29a63c
Xaurav18 <bajracharyasaurav22@gmail.com>
2021-12-03 09:21:08 +0545

    day 3

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,18 +1,35 @@
 import React from "react";
 import styled from "@emotion/styled";
+import { Notifications, Search } from "@mui/icons-material";
 
 const NavBar = styled.div`
   background-color: #0b0b0b;
   color: white;
+  font-size: 14px;
+  padding: 0px 50px;
 `;
+const HeaderImg = styled.img`
+  height: 25px;
+`;
+const HeaderLogin = styled.div``;
 
 function Navbar() {
   return (
     <NavBar>
-      <img
+      <HeaderImg
         src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
         alt=""
       />
+      <span>Homepage</span>
+      <span>Series</span>
+      <span>MOvies</span>
+      <span>New and Populalr</span>
+      <span>My List</span>
+      <HeaderLogin>
+        <Search />
+        <span>KID</span>
+        <Notifications />
+      </HeaderLogin>
     </NavBar>
   );
 }
